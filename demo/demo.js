85b8ffcc79f3ec115c847dc9f101e0c011e64583
Blake Haswell <haswell00@gmail.com>
2012-10-25 20:37:12 +1100

    Added some code to demonstrate the event being made available.

--- demo/demo.js
+++ demo/demo.js
@@ -6,10 +6,12 @@ behaviour.add("demo", {
     events: {
         mouseenter: function () {
             this.$elem.css("background-color", "#f00");
+            this.$elem.text(this.event.type);
         },
         
         mouseleave: function () {
             this.$elem.css("background-color", "#00f");
+            this.$elem.text(this.event.type);
         }
     }
 });
